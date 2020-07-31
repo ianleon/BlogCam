43f5670bbaded6d87ce6d11c6bff0c6fe7be8c48
Ian Leon <iannleon@me.com>
2020-07-31 12:04:12 -0400

    Adds a basic viewfinder

--- BlogCam/ContentView.swift
+++ BlogCam/ContentView.swift
@@ -1,15 +1,64 @@
-//
-//  ContentView.swift
-//  BlogCam
-//
-//  Created by Ian Leon on 7/31/20.
-//
-
 import SwiftUI
+import AVKit
+
+class LegacyViewfinder: UIView
+{
+
+    // We need to set a type for our layer
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+}
+
+struct Viewfinder:UIViewRepresentable {
+    
+    var session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> UIView {
+        let legacyView = LegacyViewfinder()
+        PREVIEW : if let previewLayer = legacyView.layer as? AVCaptureVideoPreviewLayer {
+            previewLayer.session = session
+        }
+        return legacyView
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+        // TODO: Handle orientation updates
+    }
+    
+    typealias UIViewType = UIView
+}
 
 struct ContentView: View {
+    var session = AVCaptureSession()
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        // START Setting configuration properties
+        session.beginConfiguration()
+
+        
+        // Get the capture device
+        DEVICE : if let frontCameraDevice = AVCaptureDevice.default(
+            .builtInWideAngleCamera,
+            for: .video,
+            position: .front
+        ) {
+
+            // Set the capture device
+            do {
+                try! session.addInput(AVCaptureDeviceInput(device: frontCameraDevice))
+            }
+        }
+
+        // END Setting configuration properties
+        session.commitConfiguration()
+
+        // Start the AVCapture session
+        session.startRunning()
+        
+        return Viewfinder(session: session)
     }
 }
 
