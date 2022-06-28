abe968af77ee624417493597781dc3ba7ccdba1b
Ian Leon <iannleon@me.com>
2022-06-27 22:19:25 -0400

    Removes size property

--- BlogCam/Icon.swift
+++ BlogCam/Icon.swift
@@ -11,27 +11,20 @@ struct Icon: View {
     let systemImageName: String
     let accessibilityLabel: String
     let action: () -> Void
-    let size: CGFloat
-    
-    enum Sizes: CGFloat {
-        case normal = 36
-    }
     
     init(
         label accessibilityLabel: String,
         _ systemImageName: String = "a",
-        _ size: Sizes = .normal,
         action: @escaping () -> Void = { }
     ) {
         self.systemImageName = systemImageName
         self.accessibilityLabel = accessibilityLabel
         self.action = action
-        self.size = size.rawValue
     }
     
     fileprivate func icon() -> some View {
         Image(systemName: systemImageName)
-            .font(.system(size: size))
+            .font(.system(size: 36))
     }
     
     var body: some View {
